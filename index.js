3f457b684a806b5f823ce801b8fc511ceeb264c7
Oscar Linderholm <osm@recv.se>
2022-10-25 12:13:15 +0200

    Allow Infinity

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const kLimits = Symbol('limits')
 function parseLimit (limits, key, defaultValue) {
   const input = limits[key] == null ? defaultValue : limits[key]
   const value = bytes.parse(input)
+  if (value === Infinity) return value
   if (!Number.isFinite(value)) throw new Error(`Invalid limit "${key}" given: ${limits[key]}`)
   if (!Number.isInteger(value)) throw new Error(`Invalid limit "${key}" given: ${value}`)
   return value
